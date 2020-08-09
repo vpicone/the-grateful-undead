31c14d485d47393b64c89a781faafbf63bbd5088
Vince Picone <Vincent.Patrick.Picone@ibm.com>
2020-08-09 17:31:38 -0500

    add og

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,16 @@
+import Head from "next/head";
+
 const Homepage = () => (
   <main>
+    <Head>
+      <title>The Grateful Undead</title>
+      <meta property="og:title" content="The Grateful Undead"></meta>
+      <meta
+        property="og:description"
+        content="The Grateful Undead - rulers of Tichondrius"
+      ></meta>
+      <meta property="og:image" content="/og.png"></meta>
+    </Head>
     <h1>The Grateful Undead</h1>
   </main>
 );
